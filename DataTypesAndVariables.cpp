0868d6231226a1b02375b5a35a61be6e8d489e5e
Jay-Mills <jay.mills@student.sl.on.ca>
2024-09-16 13:06:45 -0400

    Second Commit for testing purposes

---
+++ DataTypesAndVariables.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int num1 = 5;
+	char let1 = 'd';
+	string emp01 = "John";
+	bool isTrue = false;
+
+}
---
+++ Practice.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+class employee {
+public:
+	string name, company;
+	int age, yearsExperience;
+
+	employee() {
+
+	}
+};
+
+int main() {
+
+
+}
